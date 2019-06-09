171945d44d57408a14d91454827e38a4036917e1
soup-bowl <code@soupbowl.io>
2019-06-09 20:56:12 +0100

    Consistent line-endings.

--- WinPHP.ps1
+++ WinPHP.ps1
@@ -2,20 +2,21 @@ param (
 	[Parameter(Mandatory=$true)][string]$Operation,
 	[string]$Version
 )
-New-Variable -Name "WinPHPLoc" -Value "https://windows.php.net/downloads/releases"
+New-Variable -Name "WinPHPLoc" -Value "https://windows.php.net/downloads/releases";
 
 Function Get-Version {
-	Write-Host "Version 0.1";
+	Write-Host "Windows PHP Manager, by soup-bowl.";
+	Write-Host "Version 0.1-alpha";
 }
 
 Function Invoke-PHPDownload {
 	param( [string]$Version )
 
 	if ( ! ( Test-Path -Path $Version ) ) { 
-		Get-PHPDownload $Version
-		Expand-Archive -Path "${env:temp}\win-php\php-${Version}.zip" -DestinationPath $Version
+		Get-PHPDownload $Version;
+		Expand-Archive -Path "${env:temp}\win-php\php-${Version}.zip" -DestinationPath $Version;
 	} else {
-		Write-Output "The requested PHP version exists in this directory."
+		Write-Output "The requested PHP version exists in this directory.";
 	}
 }
 
@@ -28,42 +29,42 @@ Function Get-PHPDownload {
 	# Grab the PHP version suited to the current architecture.
 	New-Variable -Name "CurrentArch"
 	if ( [Environment]::Is64BitOperatingSystem ) {
-		Set-Variable -Name "CurrentArch" -Value "x64"
+		Set-Variable -Name "CurrentArch" -Value "x64";
 	} else {
-		Set-Variable -Name "CurrentArch" -Value "x86"
+		Set-Variable -Name "CurrentArch" -Value "x86";
 	}
 
 	# Pull from Archive if requested.
-	New-Variable -Name "RequestURL"
+	New-Variable -Name "RequestURL";
 	if ( $Archive -eq $false ) {
-		Set-Variable -Name "RequestURL" -Value "${WinPHPLoc}/php-${Version}-nts-Win32-VC15-${CurrentArch}.zip"
+		Set-Variable -Name "RequestURL" -Value "${WinPHPLoc}/php-${Version}-nts-Win32-VC15-${CurrentArch}.zip";
 	} else {
-		Set-Variable -Name "RequestURL" -Value "${WinPHPLoc}/archives/php-${Version}-nts-Win32-VC15-${CurrentArch}.zip"
+		Set-Variable -Name "RequestURL" -Value "${WinPHPLoc}/archives/php-${Version}-nts-Win32-VC15-${CurrentArch}.zip";
 	}
 
 	New-Variable `
 		-Name "DownloadLink" `
-		-Value $RequestURL
+		-Value $RequestURL;
 
 	if( ! ( Test-Path -Path "${env:temp}\win-php" ) ) {
-		New-Item -Path "${env:temp}\win-php" -ItemType "directory"
+		New-Item -Path "${env:temp}\win-php" -ItemType "directory";
 	}
 
 	# Check if a cached download is available.
 	if( Test-Path -Path "${env:temp}\win-php\php-${Version}.zip" ) {
-		Write-Host "Extracting ${Version} from cache."
+		Write-Host "Extracting ${Version} from cache.";
 	} else {
 		# Attempt to download the requested PHP version.
 		try {
 			Invoke-WebRequest `
 				-Uri $DownloadLink `
 				-OutFile "${env:temp}\win-php\php-${Version}.zip" `
-				-ErrorAction Stop
+				-ErrorAction Stop;
 		} catch [System.Net.WebException] {
 			if ( $Archive -eq $false ) {
-				Get-PHPDownload $Version $true
+				Get-PHPDownload $Version $true;
 			} else {
-				Write-Host "Unable to find the requested PHP version ${Version}, either current or archived."
+				Write-Host "Unable to find the requested PHP version ${Version}, either current or archived.";
 				exit
 			}
 		}
